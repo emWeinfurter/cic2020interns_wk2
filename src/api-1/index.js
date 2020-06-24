4ab99a0d695382ef9a962b48079cb8979f114b60
Elise Weinfurter <elise.weinfurter@ibm.com>
2020-06-24 16:42:49 -0500

    api-1 desired output

--- src/api-1/index.js
+++ src/api-1/index.js
@@ -7,18 +7,25 @@ const axios = require('axios');
 const uri = 'https://api.covid19api.com/summary';
 
 app.get('/totalrecovered', async function(req, res) {
+    let objectArr = [];
     const response = await axios.get(uri)
         .then(response => {
-            const country = response.data.Countries;
-            const countryArr = country.map(item => {
-                const object = new Countries(item.Country, item.TotalRecovered);
-                console.log(object);
+            const countrySpec = response.data.Countries;
+            const countryArr = countrySpec.map(index => {
+                const country = new Countries();
+                Object.assign(country, index);
+                objectArr.push(country);
             })
         })
         .catch(error => {
             console.log(error);
         });
-    //res.send(response);
+    objectArr.sort((a, b) => (a.TotalRecovered < b.TotalRecovered) ? 1 : -1);
+    let outArr = [];
+    for (var i = 0; i <=10; i++){
+        outArr[i] = objectArr[i].Country;
+    }
+    res.send(outArr);
 });
 
 const server = app.listen(8081, function () {
