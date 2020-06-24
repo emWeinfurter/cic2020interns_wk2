cf782ba54467037a6665f4f860ca98d6d246f554
Elise Weinfurter <elise.weinfurter@ibm.com>
2020-06-24 17:01:58 -0500

    success api-2

--- src/api-2/index.js
+++ src/api-2/index.js
@@ -7,22 +7,25 @@ const axios = require('axios');
 const uri = 'https://api.covid19api.com/summary';
 
 app.get('/newconfirmed', async function(req, res) {
+    let objectArr = [];
     const response = await axios.get(uri)
         .then(response => {
-            const country = response.data.Countries;
-            const countryArr = country.map(item => {
-                const object = new Countries(item.Country, item.NewConfirmed);
-                console.log(object);
+            const countrySpec = response.data.Countries;
+            const countryArr = countrySpec.map(index => {
+                const country = new Countries();
+                Object.assign(country, index);
+                objectArr.push(country);
             })
         })
         .catch(error => {
             console.log(error);
         });
-    //res.send(response);
+    objectArr.sort((a, b) => (a.NewConfirmed > b.NewConfirmed) ? 1 : -1);
+    res.send(objectArr.slice(0, 10));
 });
 
 const server = app.listen(8081, function () {
     const port = server.address().port;
     
-    console.log("API 2 listening at http://localhost:%s/newconfirmed", port);
+    console.log("API 1 listening at http://localhost:%s/newconfirmed", port);
 });
